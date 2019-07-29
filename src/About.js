f4bde55d2ee1105f2d367104e8979e44387d0a17
Yash Naik <yashnaik47@gmail.com>
2019-07-29 22:02:11 +0530

    initial commit

---
+++ src/About.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+    return (
+        <div>
+          <h2>Waiting for the perfect is never as smart as making progress.</h2>
+        </div>
+    );
+  }
+}
+
+export default About;
---
+++ src/Contact.js
@@ -0,0 +1,15 @@
+// Contact.js
+
+import React, { Component } from 'react';
+
+class Contact extends Component {
+  render() {
+    return (
+        <div>
+          <h2>You can send us an email at fistbumpdigital@gmail.com</h2>
+        </div>
+    );
+  }
+}
+
+export default Contact;
